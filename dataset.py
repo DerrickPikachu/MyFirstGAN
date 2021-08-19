7ab4d6aa9bb377e9d12fb51de7464bbac03b37a6
DerrickPikachu <derrfgh4563@gmail.com>
2021-08-19 15:52:42 +0800

    dataset.py add comment, complete the ICLEVRLoader

--- dataset.py
+++ dataset.py
@@ -7,12 +7,15 @@ import os
 import numpy as np
 
 
-def get_iCLEVR_data(root_folder,mode):
+def get_iCLEVR_data(root_folder, mode):
     if mode == 'train':
-        data = json.load(open(os.path.join(root_folder, 'train.json')))
+        # Read the data from json file
+        img_data = json.load(open(os.path.join(root_folder, 'train.json')))
         obj = json.load(open(os.path.join(root_folder, 'objects.json')))
-        img = list(data.keys())
-        label = list(data.values())
+        img = list(img_data.keys())
+        label = list(img_data.values())
+        # Change the string label to the one hot class vector
+        # according to the object.json file
         for i in range(len(label)):
             for j in range(len(label[i])):
                 label[i][j] = obj[label[i][j]]
@@ -21,9 +24,9 @@ def get_iCLEVR_data(root_folder,mode):
             label[i] = tmp
         return np.squeeze(img), np.squeeze(label)
     else:
-        data = json.load(open(os.path.join(root_folder, 'test.json')))
+        img_data = json.load(open(os.path.join(root_folder, 'test.json')))
         obj = json.load(open(os.path.join(root_folder, 'objects.json')))
-        label = data
+        label = img_data
         for i in range(len(label)):
             for j in range(len(label[i])):
                 label[i][j] = obj[label[i][j]]
@@ -37,16 +40,41 @@ class ICLEVRLoader(data.Dataset):
     def __init__(self, root_folder, trans=None, cond=False, mode='train'):
         self.root_folder = root_folder
         self.mode = mode
+        # img_list contain the filename when the mode is train,
+        # otherwise it will just be None
+        # label_list contain the one hot encoding vector label
         self.img_list, self.label_list = get_iCLEVR_data(root_folder, mode)
         if self.mode == 'train':
             print("> Found %d images..." % (len(self.img_list)))
-        
+
+        # TODO: What is cond used for?
         self.cond = cond
         self.num_classes = 24
-                
+
     def __len__(self):
-        """'return the size of dataset"""
-        pass
+        """return the size of dataset"""
+        return len(self.label_list)
 
     def __getitem__(self, index):
-        pass
+        torch_img = None
+
+        if self.mode == 'train':
+            img = Image.open('./images/' + self.img_list[index])
+            resized_img = img.convert('RGB').resize((64, 64))
+            narray = np.array(resized_img)
+            torch_img = torch.from_numpy(narray)
+            label = self.label_list[index]
+        else:
+            label = self.label_list[index]
+
+        return torch_img, label
+
+
+if __name__ == '__main__':
+    dataset = ICLEVRLoader('./jsonfile/')
+    torch_img, _ = dataset[10]
+    array = torch_img.numpy()
+    import matplotlib.pyplot as plt
+    plt.imshow(array)
+    plt.show()
+    # img.show()
